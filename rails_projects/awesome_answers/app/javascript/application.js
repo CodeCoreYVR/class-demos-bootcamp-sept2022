be20b0542002b3cf893ccdf6779f66c3cd798168
Stephanie <steph_war@yahoo.ca>
2022-10-27 11:11:11 -0700

    rails add js functionality to bootstrap and create bootstrap navbar

--- rails_projects/awesome_answers/app/javascript/application.js
+++ rails_projects/awesome_answers/app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import "popper"
+import "bootstrap"
+
+let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+
+
