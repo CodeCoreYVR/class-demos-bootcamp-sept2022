f58f605061c4e44604edab88d38940cc04dc049e
Stephanie <steph_war@yahoo.ca>
2022-11-02 10:28:04 -0700

    rails many to many: validation

--- rails_projects/awesome_answers/app/models/like.rb
+++ rails_projects/awesome_answers/app/models/like.rb
@@ -1,4 +1,23 @@
 class Like < ApplicationRecord
+    #To create this model after already creating a migration for the likes join table, run:
+    # rails g model like --skip
+
+    #Associations
     belongs_to :user
     belongs_to :question
+
+    #Validations
+
+    validates(
+        :question_id,
+        uniqueness: {
+            scope: :user_id,
+            message: "Has already been liked"
+        }
+    )
+
+    #If we did not have the scope, then this valiodation would have limited a question to only be liked once
+    #but now the questions can be liked multiple times, but only once by a particular user
+
+
 end
