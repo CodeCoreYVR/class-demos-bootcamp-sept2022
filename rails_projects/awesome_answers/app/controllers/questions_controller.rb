a5dad7ed2858b1bcc75bde9c8d77cef65713aba1
Stephanie <steph_war@yahoo.ca>
2022-11-03 10:27:02 -0700

    rails more on many to many: add tags to views and question create

--- rails_projects/awesome_answers/app/controllers/questions_controller.rb
+++ rails_projects/awesome_answers/app/controllers/questions_controller.rb
@@ -93,7 +93,7 @@ class QuestionsController < ApplicationController
   # to inject data for the required params in the form,
   # related to the given model:
   def question_params
-    params.require(:question).permit(:title, :body)
+    params.require(:question).permit(:title, :body, tag_ids: [])
     # get the data from the form and add it into the DB
     # Use the 'require' on the params object to retrieve the nested hash of a key
     # usually corresponding the key-value pairs of a form
