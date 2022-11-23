734f46ecdecda6477375b418de3c4ec9f7749097
Stephanie <steph_war@yahoo.ca>
2022-11-23 09:59:55 -0800

    react basics update hello_world client to account for react 18.2

--- react_basics/hello_world_react/main.js
+++ react_basics/hello_world_react/main.js
@@ -17,16 +17,24 @@ const ourFirstElement = React.createElement(
 //Because the main.js script will be loaded before the html body, we need to make
 //sure that the document's content is loaded before we refer to it
 //(i.e. if you need to grab a div, in this case) To do this, we wrap it into an event listener:
+// document.addEventListener('DOMContentLoaded', () => {
+//     //ReactDOM library is used to interpret React elements and render them to the DOM
+//     //with the .render method
+//     ReactDOM.render(
+//         //first argument is a React element - this is returned from the React.createElement
+//         ourFirstElement,
+//         // newElementWithProps,
+//         //second argument is a DOM node we want to attach the element to
+//         //in this case we are going to grab the div we created with the id of "app"
+//         document.getElementById("app")
+//     )
+// })
+
 document.addEventListener('DOMContentLoaded', () => {
-    //ReactDOM library is used to interpret React elements and render them to the DOM
-    //with the .render method
-    ReactDOM.render(
-        //first argument is a React element - this is returned from the React.createElement
-        ourFirstElement,
-        // newElementWithProps,
-        //second argument is a DOM node we want to attach the element to
-        //in this case we are going to grab the div we created with the id of "app"
-        document.getElementById("app")
+    const container = document.getElementById("app");
+    const root = ReactDOM.createRoot(container);
+    root.render(
+        ourFirstElement
     )
 })
 
