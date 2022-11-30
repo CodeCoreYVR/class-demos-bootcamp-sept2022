23f49c80d73999954d03da532dc01acae60b8b21
Stephanie <steph_war@yahoo.ca>
2022-11-30 09:56:10 -0800

    js webpack add initial config and build scripts

--- js_webpack/webpack_demo/src/index.js
+++ js_webpack/webpack_demo/src/index.js
@@ -17,3 +17,5 @@ console.log("This is the index.js entry point")
 console.log("Goodbye World!")
 
 console.log("This does not run automatically if I don't have the --watch running")
+
+console.log("Added mode config to use development environment!")
