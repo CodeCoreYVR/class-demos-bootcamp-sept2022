0098ac9e5503b83bc4715016b7dae52273ea44df
Stephanie <steph_war@yahoo.ca>
2022-11-30 09:45:31 -0800

    js webpack set up npm project and entry and output points

---
+++ js_webpack/webpack_demo/src/index.js
@@ -0,0 +1,19 @@
+import "./a.js"
+
+console.log("Hello World");
+
+function add(a,b){
+    return a + b;
+}
+
+add(1,2);
+
+const arr = [1,2,3,4]
+
+add(arr[0], arr[2])
+
+console.log("This is the index.js entry point")
+
+console.log("Goodbye World!")
+
+console.log("This does not run automatically if I don't have the --watch running")
