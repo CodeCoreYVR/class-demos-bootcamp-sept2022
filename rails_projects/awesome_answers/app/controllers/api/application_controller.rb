c27b5af62ac58bb7a72b70cc4407ff9563c40826
Stephanie <steph_war@yahoo.ca>
2022-12-01 10:00:46 -0800

    rails api errors StandardError for generic errors

--- rails_projects/awesome_answers/app/controllers/api/application_controller.rb
+++ rails_projects/awesome_answers/app/controllers/api/application_controller.rb
@@ -1,6 +1,17 @@
 class Api::ApplicationController < ApplicationController
     skip_before_action :verify_authenticity_token
 
+    #rescue_from PRIORITY
+    #The priority of rescue_from is in the reverse order of where the calls are made,
+    #meaning that the more specific erorrs should be rescued last and general errors
+    #(like StandardError) should be recued first
+
+    #The StandardError class is an ancestor of all the errors that programmers could possibly 
+    #cause in their program.  Rescuing from it will prevent nearly all errors from crashing the program
+    #Note: Use this very carefully and make sure to always log the error messages in some form
+
+    rescue_from StandardError, with: :standard_error
+
     def not_found
         render(
             json: {
@@ -20,5 +31,24 @@ class Api::ApplicationController < ApplicationController
         unless current_user.present?
             render(json:{status:401})
         end
+    end
+
+    protected
+
+    def standard_error(error)
+        logger.error error.full_message
+
+        render(
+            status: 500, #alias :internal_server_error
+            json: {
+                errors: [
+                    {
+                        type: error.class.to_s,
+                        message: error.message
+                    }
+                ]
+            } 
+        )
+
     end
 end
