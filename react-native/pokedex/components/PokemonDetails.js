fd1c50e46574be27a212d2f3150563659b565059
Matt <mattcdneale@gmail.com>
2022-12-05 12:44:46 -0800

    react-native display single pokemon details and image

--- react-native/pokedex/components/PokemonDetails.js
+++ react-native/pokedex/components/PokemonDetails.js
@@ -1,12 +1,47 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, Text, StyleSheet, Image } from 'react-native'
 
-function PokemonDetails() {
+function PokemonDetails(props) {
+  const params = props.route.params
+  const [pokemon, setPokemon] = useState({})
+
+  useEffect(() => {
+    fetch(params.pokemon.url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      setPokemon(data)
+    })
+  }, [])
+  
+
   return(
-    <View>
-      <Text>Pokemon Details</Text>
+    <View style={styles.container}>
+     <Image
+      source={{
+        uri: pokemon.id? pokemon.sprites.front_default : null
+      }} style={styles.image}>
+     </Image>
+     <Text style={styles.h1}>{pokemon.name}</Text>
+     <Text>Height: {pokemon.height}</Text>
+     <Text>Weight: {pokemon.weight}</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  },
+  h1: {
+    fontSize: 30
+  },
+  image: {
+    height: 300,
+    width: 300
+  }
+})
 
 export default PokemonDetails
