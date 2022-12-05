fab066fdf2b00317774499b132ff65894a547f54
Matt <mattcdneale@gmail.com>
2022-12-05 11:34:18 -0800

    react-native highlight pokemon when pressing it on the list

--- react-native/pokedex/components/PokeList.js
+++ react-native/pokedex/components/PokeList.js
@@ -1,5 +1,5 @@
 import { React } from "react";
-import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import { View, Text, StyleSheet, ScrollView, TouchableHighlight } from 'react-native'
 
 function PokeList({list}) {
   return(
@@ -7,9 +7,12 @@ function PokeList({list}) {
       {
         list.map((pokemon, i) => {
           return(
-            <View key={i} style={styles.pokemon}>
-              <Text>{pokemon.name}</Text>
-            </View>
+            <TouchableHighlight key={i} underlayColor='lightblue' onPress={(event)=>{console.log(event)}}>
+              <View  style={styles.pokemon}>
+                <Text>{pokemon.name}</Text>
+              </View>
+
+            </TouchableHighlight>
           )
         })
       }
